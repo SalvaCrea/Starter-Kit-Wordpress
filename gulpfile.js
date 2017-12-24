12d638f2fd53e2c640b5371c1b915d20655ca4cc
Cardona <hebustha@gmail.com>
2017-12-24 21:45:57 +0100

    ++

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,18 @@ var gulp = require('gulp');
 var request = require('request');
 var git = require('gulp-git');
 
-gulp.task('install', function() {
-			
-});
+gulp.task('install', ['cloneWordpress']);
 
-// Clone remote repo to sub folder ($CWD/sub/folder/git-test)
+// Get Wordpress by repositorie Git
 gulp.task('cloneWordpress', function() {
   git.clone('https://github.com/WordPress/WordPress', {args: './wordpress'}, function(err) {
     // handle err
   });
 });
+// Clone Folder theme in the directiry ./wordpress/wp-content/themes
+gulp.task('cloneTheme', function(){
+	gulp.src('./theme/')
+		.pipe(gulp.dest('./public/'));
+});
+
+gulp.task('install', ['cloneWordpress']);
